961a504c560e2b565c00cc5749041c4420524fa8
ksy-1024 <13437280321@163.com>
2021-04-10 20:07:46 +0800

    Dev ksy (#3) test for connect neo4j
    test for connect neo4j

--- components/data_assemble/src/lib.rs
+++ components/data_assemble/src/lib.rs
@@ -1,7 +1,51 @@
+
+
 #[cfg(test)]
 mod tests {
+    use neo4rs::*;
+    use std::sync::Arc;
+    use std::sync::atomic::{AtomicU32, Ordering};
+    use futures::stream::*;
+    use uuid::Uuid;
+    use rusted_cypher::Statement;
+
     #[test]
     fn it_works() {
         assert_eq!(2 + 2, 4);
     }
+
+    #[tokio::test]
+    async fn test_neo4j(){
+        let uri = "127.0.0.1:7687";
+        let user = "neo4j";
+        let pass = "1234.abcd";
+        let id = Uuid::new_v4().to_string();
+
+        let graph = Arc::new(Graph::new(&uri, user, pass).await.unwrap());
+        let mut result = graph.run(
+            query("CREATE (p:Person {id: $id})").param("id", id.clone())
+        ).await.unwrap();
+
+        let mut handles = Vec::new();
+        let mut count = Arc::new(AtomicU32::new(0));
+        for _ in 1..=42 {
+            let graph = graph.clone();
+            let id = id.clone();
+            let count = count.clone();
+            let handle = tokio::spawn(async move {
+                let mut result = graph.execute(
+                    query("MATCH (p:Person {id: $id}) RETURN p").param("id", id)
+                ).await.unwrap();
+                while let Ok(Some(row)) = result.next().await {
+                    count.fetch_add(1, Ordering::Relaxed);
+                }
+            });
+            handles.push(handle);
+        }
+
+        futures::future::join_all(handles).await;
+        assert_eq!(count.load(Ordering::Relaxed), 42);
+    }
+
+
 }
