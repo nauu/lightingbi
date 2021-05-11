e954ceae844c0336343f04ad5e111b3b31c99a70
zhukai <zhukai@apache.org>
2021-05-11 09:56:39 +0800

    optimization warnings

--- components/graphql/src/query_root.rs
+++ components/graphql/src/query_root.rs
@@ -1,10 +1,9 @@
 use crate::query_dataset::{MutationDataset, QueryDataset};
+use crate::query_formula::QueryFormula;
 use crate::query_user::QueryUser;
 use async_graphql::{Context, FieldResult, MergedObject, Object};
-use crud_crait::CRUD;
 use sqlx::MySqlPool;
 use user::User;
-use crate::query_formula::QueryFormula;
 
 #[derive(MergedObject, Default)]
 pub struct QueryRoot(QueryUser, QueryDataset, QueryFormula);
