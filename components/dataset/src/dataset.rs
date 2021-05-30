35d235f0703206a4110a949dfa72b892919b30ef
zhukai <zhukai@apache.org>
2021-05-31 00:15:05 +0800

    optimization warnings

--- components/dataset/src/dataset.rs
+++ components/dataset/src/dataset.rs
@@ -6,7 +6,7 @@ use serde::{Deserialize, Serialize};
 use sqlx::{FromRow, MySqlPool};
 use util_crait::uuid_util;
 
-use async_graphql::{InputObject, OutputType, Positioned, ServerResult, SimpleObject};
+use async_graphql::{InputObject, Positioned, ServerResult, SimpleObject};
 use crud_crait::entity::{Entity, MySqlRepository, Page, PageRequest};
 use std::collections::BTreeMap;
 
